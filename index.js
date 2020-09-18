3cb2b82ff3e966ad5653c304266d6956211549b9
gabrielaomoraes <gabriela.omoraes@gmail.com>
2020-09-18 15:30:56 -0300

    replying to messages

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-const Botkit = require('botkit');
 require('dotenv').config();
+const { App } = require('@slack/bolt');
 
-// Environment variables
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN || !process.env.BOT_TOKEN) {
-    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN, BOT_TOKEN and PORT in environment');
-    process.exit(1);
-} else {
-    console.log('Good job, you have the variables!');
-}
-
-const controller = Botkit.slackbot({
-    json_file_store: './db_slackbutton_slash_command/',
-    debug: true,
-    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
-})
-
-controller.configureSlackApp({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
-    scopes: ['commands', 'bot'],
-})
-
-const bot = controller.spawn({
-    token: process.env.BOT_TOKEN,
-    incoming_webhook: {
-      url: 'WE_WILL_GET_TO_THIS'
-    }
-}).startRTM();
-
-controller.setupWebserver(process.env.PORT, function(err, webserver){
-    controller.createWebhookEndpoints(controller.webserver);
-    controller.createOauthEndpoints(controller.webserver, 
-      function(err, req, res) {
-        if (err) {
-          res.status(500).send('ERROR: ' + err);
-        } else {
-          res.send('Success!');
-        }
-    });
+const bot = new App({
+  signingSecret: process.env.CLIENT_SIGNING_SECRET,
+  token: process.env.BOT_TOKEN,
 });
 
-controller.hears(['oi', 'olá'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "Olá, humano, como posso ajudar?";
+(async () => {
+  // Start the app
+  await bot.start(process.env.PORT || 8080);
 
-    bot.reply(message, helloText);
+  console.log('⚡️ Bolt app is running!');
+})();
+
+bot.event("app_mention", async ({ context, event }) => {
+
+  try{
+    await bot.client.chat.postMessage({
+    token: context.botToken,
+    channel: event.channel,
+    text: `Hey yoo <@${event.user}> you mentioned me`
+  });
+  }
+  catch (e) {
+    console.log(`error responding ${e}`);
+  }
+
 });
+
